0f33b1afdc50e0cbcea7a7419c0318adafaad1a3
Samir Khalil <samkhalil@gmail.com>
2021-05-09 00:31:39 +0800

    added parallax fx

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+let isMobile = false;
+
 //----------------check if on mobile--------------------------
 $(window).on("resize load", function () {
   if ($(window).width() <= 768) {
+    isMobile = true;
+
     //remove parallax fx
     $("#parallax-logo").removeClass("parallax-logo");
     $("#parallax").removeClass("parallax");
@@ -9,6 +13,7 @@ $(window).on("resize load", function () {
     $("#mainNav").removeClass("yellow-bg");
     $("#mainNav").addClass("green-bg");
   } else {
+    isMobile = false;
     // add parallax fx
     $("#parallax-logo").addClass("parallax-logo");
     $("#parallax").addClass("parallax");
@@ -26,9 +31,10 @@ if ($(window).width() > 768) {
     const item = document.querySelector(element);
     item.style.transform = `translateY(${distance * speed}px)`;
   }
-
   window.addEventListener("scroll", function () {
-    parallax(".parallax", window.scrollY, 0.6);
-    parallax(".parallax-logo", window.scrollY, 0.85);
+    if (isMobile === false) {
+      parallax(".parallax", window.scrollY, 0.6);
+      parallax(".parallax-logo", window.scrollY, 0.85);
+    }
   });
 }
