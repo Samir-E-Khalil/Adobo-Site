f7d599068182e8b9a65198da4612d025a61fde4f
Samir Khalil <samkhalil@gmail.com>
2021-05-08 22:53:42 +0800

    added parallax fx

--- js/scripts.js
+++ js/scripts.js
@@ -79,14 +79,3 @@
   });
 })(jQuery); // End of use strict
 
-//----------------parallax FX----------------------------------
-
-function parallax(element, distance, speed) {
-  const item = document.querySelector(element);
-  item.style.transform = `translateY(${distance * speed}px)`;
-}
-
-window.addEventListener("scroll", function () {
-  parallax(".parallax", window.scrollY, 0.6);
-  parallax(".parallax-logo", window.scrollY, 0.85);
-});
